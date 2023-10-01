aa87e111016f0557dd9740ea493a1f35853a39a4
asim <asimusman8899@gmail.com>
2023-10-01 09:48:15 -0700

    first task

---
+++ script.js
@@ -0,0 +1,37 @@
+const inputBox = document.getElementById("input-box");
+const listcontainer = document.getElementById("list-container");
+
+function Addtask() {
+    if (inputBox.value === '') {
+        alert("You must write some text!");
+    } else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listcontainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+    saveData();
+}
+
+listcontainer.addEventListener("click", function(e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        saveData();
+    } else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+function saveData() {
+    localStorage.setItem("data", listcontainer.innerHTML);
+}
+
+function showtask() {
+    listcontainer.innerHTML = localStorage.getItem("data");
+}
+
+showtask();
